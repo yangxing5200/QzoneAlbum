b5fb2ff7412260877bc39b5c4c8e1c9d5e24ccba
jason <y.jason@cjatech.com>
2015-06-03 16:44:48 +0800

    第一步：下载公开qq相册

--- QzoneAlbum/QzoneAlbum/Form1.Designer.cs
+++ QzoneAlbum/QzoneAlbum/Form1.Designer.cs
@@ -28,19 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.btnView = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
             this.txtQQ = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnView = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.gpList = new System.Windows.Forms.GroupBox();
+            this.p1 = new System.Windows.Forms.Panel();
+            this.p2 = new System.Windows.Forms.Panel();
+            this.btnLogin = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.gpList.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.txtQQ);
             this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnLogin);
             this.groupBox1.Controls.Add(this.btnView);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
@@ -49,23 +57,21 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "基本信息";
             // 
-            // groupBox2
+            // txtQQ
             // 
-            this.groupBox2.Location = new System.Drawing.Point(12, 93);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(210, 456);
-            this.groupBox2.TabIndex = 0;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "相册列表";
+            this.txtQQ.Location = new System.Drawing.Point(72, 28);
+            this.txtQQ.Name = "txtQQ";
+            this.txtQQ.Size = new System.Drawing.Size(100, 21);
+            this.txtQQ.TabIndex = 2;
             // 
-            // groupBox3
+            // label1
             // 
-            this.groupBox3.Location = new System.Drawing.Point(228, 93);
-            this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(544, 456);
-            this.groupBox3.TabIndex = 0;
-            this.groupBox3.TabStop = false;
-            this.groupBox3.Text = "相册信息";
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "QQ号：";
             // 
             // btnView
             // 
@@ -77,21 +83,58 @@
             this.btnView.UseVisualStyleBackColor = true;
             this.btnView.Click += new System.EventHandler(this.btnView_Click);
             // 
-            // label1
+            // groupBox3
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(25, 31);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(41, 12);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "QQ号：";
+            this.groupBox3.Controls.Add(this.p2);
+            this.groupBox3.Location = new System.Drawing.Point(228, 93);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(544, 456);
+            this.groupBox3.TabIndex = 0;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "相册信息";
             // 
-            // txtQQ
+            // imageList1
             // 
-            this.txtQQ.Location = new System.Drawing.Point(72, 28);
-            this.txtQQ.Name = "txtQQ";
-            this.txtQQ.Size = new System.Drawing.Size(100, 21);
-            this.txtQQ.TabIndex = 2;
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // gpList
+            // 
+            this.gpList.Controls.Add(this.p1);
+            this.gpList.Location = new System.Drawing.Point(12, 93);
+            this.gpList.Name = "gpList";
+            this.gpList.Size = new System.Drawing.Size(210, 456);
+            this.gpList.TabIndex = 0;
+            this.gpList.TabStop = false;
+            this.gpList.Text = "相册列表";
+            // 
+            // p1
+            // 
+            this.p1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.p1.Location = new System.Drawing.Point(3, 17);
+            this.p1.Name = "p1";
+            this.p1.Size = new System.Drawing.Size(204, 436);
+            this.p1.TabIndex = 0;
+            // 
+            // p2
+            // 
+            this.p2.AutoScroll = true;
+            this.p2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.p2.Location = new System.Drawing.Point(3, 17);
+            this.p2.Name = "p2";
+            this.p2.Size = new System.Drawing.Size(538, 436);
+            this.p2.TabIndex = 0;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(287, 26);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 0;
+            this.btnLogin.Text = "qq登录";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
             // 
             // Form1
             // 
@@ -99,12 +142,14 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 561);
             this.Controls.Add(this.groupBox3);
-            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.gpList);
             this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.gpList.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -112,11 +157,15 @@
         #endregion
 
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.GroupBox groupBox3;
         private System.Windows.Forms.TextBox txtQQ;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnView;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.GroupBox gpList;
+        private System.Windows.Forms.Panel p1;
+        private System.Windows.Forms.Panel p2;
+        private System.Windows.Forms.Button btnLogin;
     }
 }
 
